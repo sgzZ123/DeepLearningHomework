c8afa5c1e98b073d88e8a62c64a06174ec61d02c
sgzZ123 <2078128392@qq.com>
2019-05-27 23:04:33 +0800

    a possible version

--- trainer.py
+++ trainer.py
@@ -45,15 +45,19 @@ def train(model, criterion, optimizer, scheduler, num_epochs=25):
                 # statistics
                 running_loss += loss.item() * input_gray.size(0)
                 print('pic:',pic,'    ','single loss:',loss.item())
+
+                if phase == 'valid':
+                    if pic <= 5:
+                        save_path = {'grayscale': '/output_dir/',
+                                     'colorized': '/output_dir/'}
+                        save_name = 'epoch-{}-pic-{}.jpg'.format(epoch, pic)
+                        to_rgb(input_gray.cpu(), output.detach().cpu(), save_path=save_path,
+                               save_name=save_name)
+
                 pic += 1
 
             epoch_loss = running_loss / dataset_sizes[phase]
             print('{} Loss: {:.5f}'.format(phase, epoch_loss))
-            for input_gray, input_ab, target in dataloader['valid']:
-                save_path = {'grayscale':'D:\\outputs\\gray\\','colorized':'D:\\outputs\\color\\'}
-                save_name = '%d.jpg' % (epoch,)
-                to_rgb(input_gray.cpu(), model(input_gray).detach().cpu(),save_path=save_path,save_name=save_name)
-                break
 
             # deep copy the model
             if epoch == 0:
@@ -66,12 +70,12 @@ def train(model, criterion, optimizer, scheduler, num_epochs=25):
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    print('Best val loss: {:4f}'.format(best_loss))
+    print('Best val loss: {:6f}'.format(best_loss))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
     # 这里要加路径！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    torch.save(model.state_dict(), '/model-epoch-{}-losses-{:.3f}.pth'.format(num_epochs+1,best_loss))
+    torch.save(model.state_dict(), '/output_dir/model-epoch-{}-losses-{:.6f}.pth'.format(num_epochs+1,best_loss))
     return model
 
 if __name__ == '__main__':
@@ -80,6 +84,6 @@ if __name__ == '__main__':
     criterion = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-    model = train(model,criterion,optimizer,exp_lr_scheduler,num_epochs=3)
+    model = train(model,criterion,optimizer,exp_lr_scheduler,num_epochs=100)
 
 
---
+++ use.py
@@ -0,0 +1,22 @@
+from trainer import *
+
+
+if __name__ == '__main__':
+    pretrained = torch.load('D:\\model-epoch-101-losses-0.003039.pth',
+                            map_location=lambda storage,loc:storage)
+    model = Network()
+    model.load_state_dict(pretrained)
+    pic = 1
+    for input_gray, input_ab, target in dataloader['train']:
+        input_gray = input_gray.to(device)
+        input_ab = input_ab.to(device)
+        target = target.to(device)
+
+        output = model(input_gray)
+        save_path = {'grayscale': 'D:\\output\\gray\\',
+                     'colorized': 'D:\\output\\color\\'}
+        save_name = 'pic-{}.jpg'.format(pic)
+
+        to_rgb(input_gray.cpu(), output.detach().cpu(), save_path=save_path,
+               save_name=save_name)
+        pic += 1
